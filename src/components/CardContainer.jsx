965d1a8ce0dc1bdf1210af23b98ae0fe0805bbb6
MihailPetkov96 <mpetkov228@gmail.com>
2024-03-16 00:57:13 +0200

    Merge Card component and Container in one file

--- src/components/CardContainer.jsx
+++ src/components/CardContainer.jsx
@@ -1,11 +1,32 @@
 import { useEffect, useState } from 'react';
 
-import Card from "./Card";
 import '../styles/CardContainer.css';
+
+
 
 const CardContainer = () => {
   const [countries, setCountries] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
+  const [score, setScore] = useState(0);
+
+  const Card = ({ country }) => {
+    function handleClick(e) {
+      if (!country.isClicked) {
+        country.isClicked = true;
+        setScore(score + 1);
+      } else {
+        setScore(0);
+      }
+
+    }
+
+    return (
+      <div onClick={handleClick} className="card">
+          <img src={country.flag} alt="country flag" />
+          <p>{country.name}</p>
+      </div>
+    )
+  };
 
   useEffect(() => {
     fetch('https://restcountries.com/v3.1/all')
@@ -32,16 +53,19 @@ const CardContainer = () => {
   }
 
   return (
-    <div id="card-container">
-      {countries.map((country) => {
-        return (
-          <Card
-            key={country.ccn3}
-            country={country}
-          />
-        )
-      })}
-    </div>
+    <>
+      <h2>{score}</h2>
+      <div id="card-container">
+        {countries.map((country) => {
+          return (
+            <Card
+              key={country.ccn3}
+              country={country}
+            />
+          )
+        })}
+      </div>
+    </>
   )
 };
 
