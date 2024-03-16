81b8c7df72e4a4bfb1450d635f3441a3b6635c8b
MihailPetkov96 <mpetkov228@gmail.com>
2024-03-17 00:39:15 +0200

    Implement score keeping and score reset on failure

--- src/components/CardContainer.jsx
+++ src/components/CardContainer.jsx
@@ -2,65 +2,63 @@ import { useEffect, useState } from 'react';
 
 import '../styles/CardContainer.css';
 
+const Card = ({ country, onClick }) => {
+  
+  return (
+    <div className="card" onClick={onClick}>
+        <img src={country.flag} alt="country flag" />
+        <p>{country.name}</p>
+    </div>
+  )
+};
 
 
 const CardContainer = () => {
   const [countries, setCountries] = useState([]);
-  const [isLoading, setIsLoading] = useState(true);
   const [score, setScore] = useState(0);
-
-  const Card = ({ country }) => {
-    function handleClick(e) {
-      if (!country.isClicked) {
-        country.isClicked = true;
-        setScore(score + 1);
-      } else {
-        setScore(0);
-      }
-
-    }
-
-    return (
-      <div onClick={handleClick} className="card">
-          <img src={country.flag} alt="country flag" />
-          <p>{country.name}</p>
-      </div>
-    )
-  };
-
+  
   useEffect(() => {
     fetch('https://restcountries.com/v3.1/all')
       .then(response => response.json())
       .then(data => {
         const newData = data.slice(30, 42).map(country => {
           return {
-            ccn3: country.ccn3,
+            id: country.ccn3,
             name: country.name.common,
             flag: country.flags.png,
-            isClicked: false,
+            clicked: false,
           };
         });
         setCountries(newData);
-        setIsLoading(false);
       })
       .catch(error => console.error(error));
   }, []);
 
-  if (isLoading) {
-    return (
-      <div>Loading...</div>
-    )
+  const handleClick = (countryId) => {
+    const country = countries.find(c => c.id == countryId);
+    if (country.clicked) {
+      const updatedData = countries.map(country => {
+          return { ...country, clicked: false };  
+      });
+      setScore(0);
+      setCountries(updatedData);
+    } else {
+      country.clicked = true;
+      setScore(score + 1);
+    }
+
   }
 
   return (
     <>
-      <h2>{score}</h2>
+      <h2>Score: {score}</h2>
       <div id="card-container">
         {countries.map((country) => {
           return (
             <Card
-              key={country.ccn3}
+              key={country.id}
               country={country}
+              onClick={() => handleClick(country.id)}
             />
           )
         })}
